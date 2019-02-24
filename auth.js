09044553d0c57b12b98059532b6020d88cf58bda
ftsan <ftsan12345@gmail.com>
2019-02-24 11:55:04 +0900

    ベーシック認証の認証情報を環境変数から取得するよう修正

--- auth.js
+++ auth.js
@@ -1,8 +1,9 @@
 const auth = require('basic-auth');
 
-const USERNAME = 'slt';
-const PASSWORD = 'slt';
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASS;
 module.exports = (request, response, next) => {
+    if (!USERNAME || !PASSWORD) return next();
     const user = auth(request);
     if (!user || user.name != USERNAME || user.pass != PASSWORD) {
         response.set('WWW-Authenticate', 'Basic realm="Restricted"');
